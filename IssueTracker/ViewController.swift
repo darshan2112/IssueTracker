bd9d2fd5cf6efcfdec23b1cc317a367dc36108d5
Darshan Shivakumar <darshan.ks@onmobile.com>
2018-10-27 11:14:52 +0530

    Initial Commit

---
+++ IssueTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by Darshan K S on 27/10/18.
+//  Copyright © 2018 IssueTracker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
